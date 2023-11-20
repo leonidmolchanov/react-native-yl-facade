037146afb676f19279dfcbeec96e384c990ecfce
Leonid Molchanov <l.molchanov@yakovlev.studio>
2023-11-20 19:52:05 +0300

    refactor: init module

--- ios/YlFacade.mm
+++ ios/YlFacade.mm
@@ -1,6 +1,8 @@
+#import "React/RCTViewManager.h"
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(YlFacade, NSObject)
+
+@interface RCT_EXTERN_MODULE(YlFacade, RCTViewManager)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
--- ios/YlFacade.swift
+++ ios/YlFacade.swift
@@ -1,8 +1,34 @@
 @objc(YlFacade)
-class YlFacade: NSObject {
+class YlFacade: RCTViewManager {
+
+    private let childView =  CustomView();
+ override func view() -> UIView! {
+          return childView
+        }
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
   }
+}
+
+
+class CustomView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(_ rect: CGRect) {
+        guard let ctx = UIGraphicsGetCurrentContext() else { return }
+        ctx.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        let rectangle = CGRect(x: 0, y: 100, width: 320, height: 100)
+        ctx.stroke(rectangle)
+    }
+
 }
